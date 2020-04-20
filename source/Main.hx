b3cea1e65a4c389f92bb872fe7850a45adc563c5
austin <eastin93@gmail.com>
2020-04-19 23:47:27 -0400

    turn off splash screen

--- source/Main.hx
+++ source/Main.hx
@@ -9,7 +9,7 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, BaseState));
+		addChild(new FlxGame(0, 0, BaseState, 1, 60, 60, true));
 		FlxG.autoPause = false;
 	}
 }
